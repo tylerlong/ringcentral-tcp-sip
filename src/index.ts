80b6e56f48071c4091f5622d4373bf0ae38d2be4
Tyler Long <tyler4long@gmail.com>
2021-01-22 22:01:34 -0800

    Refactor code

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import {Socket} from 'net';
 import waitFor from 'wait-for-async';
 import {v4 as uuid} from 'uuid';
 import {SIPInfoResponse} from '@rc-ex/core/lib/definitions';
+import {SipMessage} from './SIP';
 
 const rc = new RingCentral({
   clientId: process.env.RINGCENTRAL_CLIENT_ID,
@@ -12,19 +13,22 @@ const rc = new RingCentral({
 });
 
 const register = (client: Socket, sipInfo: SIPInfoResponse) => {
-  const body =
-    `REGISTER sip:${sipInfo.domain} SIP/2.0
-Call-ID: ${uuid()}
-User-Agent: ringcentral-tcp-sip-demo
-Contact: <sip:${uuid()}@${uuid()}.invalid;transport=tcp>;expires=600
-Via: SIP/2.0/TCP ${uuid()}.invalid;branch=${'z9hG4bK' + uuid()}
-From: <sip:${sipInfo.username}@${sipInfo.domain}>;tag=${uuid()}
-To: <sip:${sipInfo.username}@${sipInfo.domain}>
-CSeq: 8082 REGISTER
-Content-Length: 0
-Max-Forwards: 70`
-      .split(/[\n\r]+/)
-      .join('\r\n') + '\r\n\r\n';
+  const sipMessage = new SipMessage({
+    subject: `REGISTER sip:${sipInfo.domain} SIP/2.0`,
+    headers: {
+      'Call-ID': uuid(),
+      'User-Agent': 'ringcentral-tcp-sip-demo',
+      Contact: `<sip:${uuid()}@${uuid()}.invalid;transport=tcp>;expires=600`,
+      Via: `SIP/2.0/TCP ${uuid()}.invalid;branch=${'z9hG4bK' + uuid()}`,
+      From: `<sip:${sipInfo.username}@${sipInfo.domain}>;tag=${uuid()}`,
+      To: `<sip:${sipInfo.username}@${sipInfo.domain}>`,
+      CSeq: '8082 REGISTER',
+      'Content-Length': '0',
+      'Max-Forwards': '70',
+    },
+    body: '',
+  });
+  const body = sipMessage.toString();
   console.log(body);
   client.write(body);
 };
@@ -57,14 +61,19 @@ Max-Forwards: 70`
     console.log('Connected');
     register(client, sipInfo);
   });
-  client.on('data', data => {
-    console.log('Received', data);
+  client.on('data', buffer => {
+    const message = buffer.toString();
+    console.log(
+      '>>>>>> Start of received message >>>>>>\n',
+      message,
+      '\n<<<<<< End of received message <<<<<<'
+    );
   });
   client.on('close', () => {
     console.log('Closed');
   });
 
-  await waitFor({interval: 5000});
+  await waitFor({interval: 3000});
   client.destroy();
 
   await rc.revoke();

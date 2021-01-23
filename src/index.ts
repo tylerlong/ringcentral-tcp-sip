2f18a2fe870b69cee06e3d6e758ef428390feb58
Tyler Long <tyler4long@gmail.com>
2021-01-22 18:27:26 -0800

    Receive data from remote

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,32 @@ import RingCentral from '@rc-ex/core';
 import {hostname} from 'os';
 import {Socket} from 'net';
 import waitFor from 'wait-for-async';
+import {v4 as uuid} from 'uuid';
+import {SIPInfoResponse} from '@rc-ex/core/lib/definitions';
 
 const rc = new RingCentral({
   clientId: process.env.RINGCENTRAL_CLIENT_ID,
   clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
   server: process.env.RINGCENTRAL_SERVER_URL,
 });
+
+const register = (client: Socket, sipInfo: SIPInfoResponse) => {
+  const body =
+    `REGISTER sip:${sipInfo.domain} SIP/2.0
+Call-ID: ${uuid()}
+User-Agent: ringcentral-tcp-sip-demo
+Contact: <sip:${uuid()}@${uuid()}.invalid;transport=tcp>;expires=600
+Via: SIP/2.0/TCP ${uuid()}.invalid;branch=${'z9hG4bK' + uuid()}
+From: <sip:${sipInfo.username}@${sipInfo.domain}>;tag=${uuid()}
+To: <sip:${sipInfo.username}@${sipInfo.domain}>
+CSeq: 8082 REGISTER
+Content-Length: 0
+Max-Forwards: 70`
+      .split(/[\n\r]+/)
+      .join('\r\n') + '\r\n\r\n';
+  console.log(body);
+  client.write(body);
+};
 
 (async () => {
   await rc.authorize({
@@ -35,6 +55,7 @@ const rc = new RingCentral({
   const client = new Socket();
   client.connect(parseInt(port), host, () => {
     console.log('Connected');
+    register(client, sipInfo);
   });
   client.on('data', data => {
     console.log('Received', data);
@@ -43,8 +64,8 @@ const rc = new RingCentral({
     console.log('Closed');
   });
 
-  await waitFor({interval: 10000});
-  client.end();
+  await waitFor({interval: 5000});
+  client.destroy();
 
   await rc.revoke();
 })();
